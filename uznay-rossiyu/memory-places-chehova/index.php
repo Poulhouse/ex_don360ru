a0342e563bbae97e3e20b1e8fa36b4f0b0a173d4
Poulhouse <karpenko.p.s@gmail.com>
2021-11-16 02:53:27 +0300

    add final memory from ur

---
+++ uznay-rossiyu/memory-places-chehova/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Главная</title>
+    <link rel="stylesheet" href="../assets/css/nicepage.css" media="screen">
+<link rel="stylesheet" href="../assets/css/main.css" media="screen">
+    <script class="u-script" type="text/javascript" src="../assets/js/jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="../assets/js/nicepage.js" defer=""></script>
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    
+    <style>
+		.u-section-1 .u-image-1 {
+			  background-image: url("../assets/images/chehov.jpg");
+			  background-position: 50% 50%;
+			  min-height: 475px;
+		}
+	</style>
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "index.html"
+}</script>
+    <meta property="og:title" content="Главная">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+  </head>
+  <body class="u-body">
+    <section class="u-align-center u-clearfix u-section-1" id="carousel_5f49">
+      <div class="u-clearfix u-gutter-0 u-layout-wrap u-layout-wrap-1">
+        <div class="u-gutter-0 u-layout">
+          <div class="u-layout-row">
+            <div class="u-container-style u-image u-layout-cell u-left-cell u-size-28 u-image-1" data-image-width="640" data-image-height="497">
+              <div class="u-container-layout"></div>
+            </div>
+            <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-32 u-layout-cell-2">
+              <div class="u-container-layout u-container-layout-2">
+                <p class="u-text u-text-body-color u-text-1">
+                  <span style="font-size: 2.25rem; font-weight: 700;">Чехов<br>Антон Павлович</span>
+                  <br>
+                  <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar elementum integer enim neque.
+                </p>
+                <div class="u-clearfix u-expanded-width u-gutter-0 u-layout-wrap u-layout-wrap-2">
+                  <div class="u-layout">
+                    <div class="u-layout-row">
+                      
+					  <div class="u-align-left u-container-style u-layout-cell u-size-26 u-layout-cell-3">
+                        <div class="u-container-layout u-valign-middle u-container-layout-3">
+                          <a href="tour.php?audiogid=1" class="u-btn u-button-style u-hover-palette-1-dark-1 u-palette-1-base u-btn-1">Запустить аудиогид</a>
+                        </div>
+                      </div>
+					  
+                      <div class="u-align-left u-container-style u-layout-cell u-size-34 u-layout-cell-4">
+                        <div class="u-container-layout u-valign-middle u-container-layout-4">
+                          <a href="tour.php?audiogid=0" class="u-border-2 u-border-black u-btn u-button-style u-hover-black u-none u-text-black u-text-hover-white u-btn-2">Самостоятельный просмотр</a>
+                        </div>
+                      </div>
+					  
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
