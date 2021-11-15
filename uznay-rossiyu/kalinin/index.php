8ddc9669da7a73dcdbc142d707451230d31e573b
Poulhouse <karpenko.p.s@gmail.com>
2021-11-16 02:46:42 +0300

    add kalinin files from ur

---
+++ uznay-rossiyu/kalinin/index.php
@@ -0,0 +1,403 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Виртуальная экскурсия по Раздорскому этнографическому музею-заповеднику</title>
+    <link rel="stylesheet" href="../assets/css/nicepage.css" media="screen">
+<link rel="stylesheet" href="../assets/css/main.css" media="screen">
+    <script class="u-script" type="text/javascript" src="../assets/js/jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="../assets/js/nicepage.js" defer=""></script>
+    	<link href='https://fonts.googleapis.com/css2?family=Roboto+Slab' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css2?family=Open+Sans' rel='stylesheet' type='text/css'>
+    
+   <style>
+		.u-section-1 .u-image-1 {
+			  background-image: url("../assets/images/chehov.jpg");
+			  background-position: 50% 50%;
+			  min-height: 475px;
+		}
+		.body {
+			background: #fff;
+			margin: 0;
+			padding: 0;
+		}
+#centerLayer {
+    position: absolute;
+    width: 50%;
+    right: 0;
+    /* left: 50%; */
+    /* bottom: 0; */
+    /* margin-left: -440px; */
+    /* margin-top: -320px; */
+    background: ;
+    /* text-align: center; */
+    /* border: solid 0px black; */
+    /* padding: 10px; */
+    /* overflow: auto; */
+    /* padding-bottom: 350px; */
+}
+		#footer {
+			text-align: center;
+			width: 100%;
+			position: absolute;
+			bottom: 0;
+			background-color: #ce4944;
+		}
+		.l-submain {
+			width: 100%;
+			margin: 0 auto;
+			padding: 20px 40px;
+			position: relative;
+			background-position: center center;
+			-webkit-background-size: cover;
+			background-size: cover;
+			border-top: 1px solid #e8e8e8;
+		}
+		
+		.l-submain-h {
+			width: 95%;
+			/*max-width: 1000px;*/
+			margin: 0 auto;
+		}
+		.g-cols {
+			zoom: 1;
+		}
+		.g-cols > div {
+			float: left;
+			width: 100%;
+		}
+		.g-cols > div {
+			margin-left: 4.0000%;
+		}
+		.g-cols > div:first-child, .g-cols > div.full-width {
+			margin-left: 0;
+		}
+		.g-cols:before, .g-cols:after {
+			display: table;
+			line-height: 0;
+			content: "";
+		}
+		.g-cols:before, .g-cols:after {
+			display: table;
+			line-height: 0;
+			content: "";
+		}
+		.g-cols:after {
+			clear: both;
+		}
+		.w-actionbox {
+			margin-bottom: 0;
+			padding: 30px 40px;
+			border-radius: 3px;
+		}
+		.w-actionbox-h {
+			text-align: center;
+			position: relative;
+			width: 100%;
+			zoom: 1;
+		}
+		.w-actionbox.controls_aside .w-actionbox-h {
+			display: table;
+			text-align: left;
+		}
+		.w-actionbox-h:before, .w-actionbox-h:after {
+			display: table;
+			content: " ";
+		}
+		.w-actionbox-h:after {
+			clear: both;
+		}
+		.w-actionbox.controls_aside .w-actionbox-text {
+			padding: 0;
+		}
+		.w-actionbox.controls_aside .w-actionbox-text {
+			display: table-cell;
+			vertical-align: middle;
+		}
+		.w-actionbox-controls {
+			font-size: 0;
+			margin-top: 20px;
+		}
+		.w-actionbox-controls {
+			margin-top: 30px;
+		}
+		.w-actionbox.controls_aside .w-actionbox-controls {
+			display: table-cell;
+			vertical-align: middle;
+			text-align: right;
+			white-space: nowrap;
+			padding-left: 40px;
+			margin: 0;
+		}
+		.color_primary .w-actionbox.color_primary {
+			padding: 0;
+			background-color: transparent;
+		}
+		.color_primary {
+			background-color: #ce4944;
+			color: #fff;
+		}
+		.color_primary h1, .color_primary h2, .color_primary h3, .color_primary h4, .color_primary h5, .color_primary h6 {
+			color: #fff;
+			text-shadow: 0 1px 0 rgb(0 0 0 / 10%);
+		}
+		.g-html h2:first-child, .g-html h3:first-child, .g-html h4:first-child, .g-html h5:first-child, .g-html h6:first-child, .g-html h1 + h2, .g-html h2 + h3, .g-html h3 + h4, .g-html h4 + h5, .g-html h5 + h6 {
+			padding-top: 0;
+		}
+		h1 {
+			font: 21px/31px 'Roboto Slab', sans-serif !important;
+			margin-bottom: 18px !important;
+		}
+		h3 {
+			font: 28px/31px 'Open Sans', sans-serif !important;
+			text-align: center !important;
+			text-transform: lowercase !important;
+		}
+		h2 {
+			font: 38px/40px 'Roboto Slab', sans-serif !important;
+			text-align: center !important;
+		}
+		body, p, td {
+			font: 14px/24px 'Open Sans', sans-serif;
+		}
+		.w-actionbox-text h3 + p {
+			margin: 12px 0 0;
+		}
+		
+.g-btn {
+    display: inline-block;
+    vertical-align: middle;
+    font-size: 15px;
+    line-height: 20px;
+    text-align: center;
+    text-decoration: none;
+    white-space: nowrap;
+    padding: 6px 20px;
+    cursor: pointer;
+    position: relative;
+}
+.w-actionbox-button {
+    margin: 0 10px;
+}
+.g-btn, input[type="submit"] {
+    font-size: 14px;
+    line-height: 22px;
+    font-weight: 700;
+    text-transform: uppercase;
+    padding: 10px 30px;
+    margin: 5px 0;
+    border: none !important;
+    border-radius: 3px;
+    -webkit-transition: all 0.25s ease;
+    transition: all 0.25s ease;
+}
+.g-btn.type_outline {
+    box-shadow: none;
+    background-color: transparent;
+    color: #666;
+}
+.g-btn.size_big, input.g-btn.size_big, button.g-btn.size_big {
+    padding: 15px 40px;
+    font-size: 16px;
+}
+.w-actionbox-button.g-btn {
+    margin: 0 10px;
+}
+.w-actionbox.controls_aside .w-actionbox-button {
+    margin: 0 0 0 20px;
+}
+.color_primary .g-btn.type_outline {
+    color: #fff;
+}
+.w-actionbox.controls_aside .w-actionbox-button:first-child {
+    margin-left: 0;
+}
+.g-btn.type_outline:hover {
+    box-shadow: none;
+    color: #fff;
+}
+.g-btn.type_outline:hover {
+    color: #ffffff;
+}
+.w-actionbox.controls_aside .w-actionbox-button:first-child {
+    margin-left: 0;
+}
+.color_primary .g-btn.type_outline:hover {
+    color: #333;
+}
+.g-btn.type_default {
+    box-shadow: 0 -2px 0 rgb(0 0 0 / 15%) inset;
+    background-color: #f0f0f0;
+    color: #666;
+}
+.g-btn.size_big, input.g-btn.size_big, button.g-btn.size_big {
+    padding: 15px 40px;
+    font-size: 16px;
+}
+.g-btn.type_default, input[type="text"], input[type="password"], input[type="email"], textarea, select, .l-submain.for_filters, .w-clients-nav:hover, .w-portfolio-item-meta, .w-pricing-item-price, .w-shortblog-entry-meta-date, .w-tabs-list, .w-tabs.layout_accordion .w-tabs-section-title:hover, .w-tags.layout_block .w-tags-item-link, .w-testimonial-text, .w-timeline-list:before, .w-timeline.type_vertical .w-timeline-section:before {
+    background-color: #f0f0f0;
+}
+.l-canvas, .g-btn.type_default, .g-btn.type_outline, .g-pagination-item, .w-clients-nav, .w-clients-nav:hover, .w-filters-item-link, .w-iconbox.icon_top .w-iconbox-h, .w-nav-list.layout_ver .w-nav-anchor, .w-pagehead h1, .w-socials-item-link, .w-tags-item-link, .widget.widget_archive ul li a, .widget.widget_categories ul li a, .widget.widget_nav_menu .menu-item a {
+    color: #666666;
+}
+.g-btn span {
+    position: relative;
+}
+.g-btn.type_outline:before {
+    display: block;
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    z-index: 0;
+    border-radius: 3px;
+    -webkit-transition: box-shadow 0.25s ease;
+    transition: box-shadow 0.25s ease;
+    box-shadow: 0 0 0 2px #333 inset;
+    opacity: 0.5;
+}
+.color_primary .g-btn.type_outline:before {
+    box-shadow: 0 0 0 2px #fff inset;
+}
+.color_primary .g-btn.type_outline:hover:before {
+    box-shadow: 0 0 0 26px #fff inset;
+}
+.g-btn.type_default:hover {
+    box-shadow: 0 0 0 26px rgb(0 0 0 / 60%) inset;
+    color: #333;
+}
+.g-html h1, .g-html h2, .g-html h3, .g-html h4, .g-html h5, .g-html h6, input[type="text"], input[type="password"], input[type="email"], textarea, select, .g-btn.type_default:hover, .w-blog-entry-title, .color_primary .w-iconbox.icon_top .w-iconbox-h .w-iconbox-text-title, .color_alternate .w-iconbox.icon_top .w-iconbox-h .w-iconbox-text-title, .w-tabs-item:hover {
+    color: #FFFFFF;
+}
+.copyright a {
+	font-size: 12px;
+    padding-right: 9px;
+    position: absolute;
+    right: 0;
+}
+@media all and (max-width:1140px) {
+	.w-actionbox.controls_aside .w-actionbox-controls {
+		display: table-row;
+		text-align: left;
+		padding-left: 0;
+	}
+	.w-actionbox.controls_aside .w-actionbox-text {
+		display: table-row;
+	}
+}
+@media all and (max-width:920px) {
+	.w-actionbox.controls_aside .w-actionbox-controls {
+		display: block;
+		text-align: left;
+		padding-left: 0;
+	}
+	.w-actionbox.controls_aside .w-actionbox-text {
+		display: block;
+	}
+	.w-actionbox.controls_aside .w-actionbox-button {
+		margin: 0;
+		float: left;
+		width: 100%;
+		font-size: 12px;
+	}
+	.w-actionbox.controls_aside .w-actionbox-button:first-child {
+		margin-left: 0;
+		margin-bottom: 20px;
+		width: 100%;
+	}
+	#centerLayer > img {
+		width: 100% !important;
+		height: auto !important;
+	}
+	.copyright a {
+		position: relative;
+		display: block;
+	}
+	.copyright span {
+		width: 100%;
+		display: block;
+	}
+	#centerLayer {
+		position: relative;
+		width: 100%;
+	}
+	.w-actionbox-h {
+		display: block !important;
+	}
+}
+	</style>
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "https://fiveday.ru/tour_new/kalinin/"
+}</script>
+    <meta property="og:title" content="Виртуальный тур по Раздорскому этнографическому музею-заповеднику">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="https://fiveday.ru/tour_new/kalinin/">
+    <meta property="og:url" content="https://fiveday.ru/tour_new/kalinin/">
+  </head>
+  
+<body class="body " style="">
+	<img src="../assets/images/fond_black.png" width="300" style="position: absolute; left: 0; top: 0; z-index: 2;"/>
+	<div id="centerLayer">
+		
+		<img src="../assets/images/kalinin.jpeg" width="500" style="position: absolute;width: auto;height: calc(100vh - 200px); 
+    right: 0;">	 
+	</div>
+
+	<div id="footer">
+		<div class="l-submain color_primary">
+			<div class="l-submain-h g-html ">
+				<div class="g-cols">
+					<div class="full-width">
+						<div class="w-actionbox color_primary controls_aside">
+							<div class="w-actionbox-h">
+								<div class="w-actionbox-text"><h1>Виртуальный тур по Раздорскому этнографическому музею-заповеднику</h1><p>Экскурсия по музею-заповеднику в сопровождении аудиогида</p></div>
+								<div class="w-actionbox-controls at_right">
+									<a class="w-actionbox-button g-btn size_big type_outline" href="tour.php?audiogid=1">
+										<span>Запустить аудиогид</span>
+									</a>
+									<a class="w-actionbox-button g-btn size_big type_default" href="tour.php?audiogid=0">
+										<span>Самостоятельный просмотр</span>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="copyright" style="background-color: #ffffff;">
+			<span>&copy; при поддержке Фонда Президентских Грантов</span>
+			<a href="https://don360.ru/">Студия виртуальных туров: DON360.RU</a>
+			
+			
+		</div>
+	</div>
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript" >
+	   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+	   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+	   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+	   ym(82300993, "init", {
+			clickmap:true,
+			trackLinks:true,
+			accurateTrackBounce:true
+	   });
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/82300993" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
+	
+</body>
+</html>
