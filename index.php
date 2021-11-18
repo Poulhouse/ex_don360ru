887283bc1b9c248cef7f21f77f7427fcfd34caf5
Poulhouse <karpenko.p.s@gmail.com>
2021-11-18 22:27:52 +0300

    add index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+		<title>History</title>
+		<link href='https://fonts.googleapis.com/css2?family=Roboto+Slab' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css2?family=Open+Sans' rel='stylesheet' type='text/css'>
+		<link href='css/style.css?v=1.0.0' rel='stylesheet' type='text/css'>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">    
+    <script type="application/ld+json">{"@context": "http://schema.org","@type": "Organization","name": "","url": "https://fiveday.ru/tour_new/rostov/"}</script>				
+		<meta property="og:title" content="Виртуальный тур по улице Пушкинской в Ростове-на-Дону">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="https://fiveday.ru/tour_new/rostov/">
+    <meta property="og:url" content="https://fiveday.ru/tour_new/rostov/">
+  </head>
+ <body class="body " style="">
+ 
+	<header id="header">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<div class="container header_wrapper">
+				<a class="navbar-brand fs-2" href="https://don360.ru">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" style="width:1em;height:1em;" class="bi bi-arrow-left-square" viewBox="0 0 16 16">
+						<path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"></path>
+					</svg> Вернуться на главную
+				</a>
+				<div class="col-auto lang_wrapper d-none d-lg-block">								
+						<div class="lang_wrap">
+							<a href="?lang=ru" class="active">
+								<img src="images/rus.jpg" class="lang_img" />
+								<p class="small_text">Русский</p>
+							</a>
+							<a href="?lang=en" class="active">
+								<img src="images/eng.jpg" class="lang_img" />
+								<p class="small_text">English</p>
+							</a>
+							<a href="?lang=ch" class="active">
+								<img src="images/china.jpg" class="lang_img" />
+								<p class="small_text">中国人</p>
+							</a>
+						</div>
+				</div>			
+			</div><!-- /.container -->	
+		</nav>
+	</header>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <script class="u-script" type="text/javascript" src="js/nicepage.js" defer=""></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+	
+</body>  
+</html>
